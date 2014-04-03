b7dc57628901b9a38ef566abb16f9c2602c78e2c
Rohan App <nareshapp@yahoo.com>
2014-04-02 23:30:27 -0700

    Delete temp bat files

--- foo.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set reponame=Repo1
-set viewdir=c:\ANSYSDev\%reponame%\nextgen
-
-call cleansln %viewdir%\ansoftcore\Core.sln Debug
-call cleansln %viewdir%\ansoftcore\Core.sln Release
-
