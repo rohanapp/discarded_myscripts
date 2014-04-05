61be6b4ac27d700c153da14172bb55afa1fbbe27
Rohan App <nareshapp@yahoo.com>
2014-04-04 23:23:36 -0700

    Add SJ artifactory settings to template

--- templateconfig.cmd
+++ templateconfig.cmd
@@ -8,9 +8,15 @@ set tempdir=\temp
 set progpath1=C:\Program Files\WinZip
 
 REM Artifactory download settings
-set artifactorysite=http://canartifactory:8080/artifactory/simple
 set username=nappann
-set userpasswd=\{DESede\}8jwH8iChqx+Kud7qnvie1A==
+
+REM HQ Artifactory
+REM set artifactorysite=http://canartifactory:8080/artifactory/simple
+REM set userpasswd=\{DESede\}8jwH8iChqx+Kud7qnvie1A==
+REM SJ Artifactory
+set artifactorysite=http://sjoartsrv01:8080/artifactory
+set userpasswd=\{DESede\}zXjI4YKQ5kyHnup5RJ5TIg==
+
 set artifact_download_dir=E:\dtemp\installs_extract\ANSYSDownloads
 set wgetdir=E:\programs\bin
 REM April 3 2014 is 20140403 (yyyymmdd)
